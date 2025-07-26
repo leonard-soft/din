bf6404538b1568ae6a37e647d5345d337efb5cde
leonard-soft <leoberfo@gmail.com>
2025-07-26 00:48:59 -0500

    feat: ask the main questions

---
+++ src/app/app.cpp
@@ -0,0 +1,33 @@
+#include "../../include/app/app.hpp"
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+/**
+ * @brief App::run method
+ * 
+ * this method has the code to initialize and configure a c++ project
+ * also this function contains the logic code to add dependencies into the 
+ * projects.
+ */
+void App::run() {
+
+    std::unique_ptr<std::string> projectName = std::make_unique<std::string>();
+    std::unique_ptr<std::string> projectDescription = std::make_unique<std::string>();
+    std::unique_ptr<std::string> author = std::make_unique<std::string>();
+    std::unique_ptr<std::string> license = std::make_unique<std::string>();
+
+    std::cout << "↳ 〔 ✾ Name ✾ 〕: ";
+    std::getline(std::cin, *projectName); 
+
+    std::cout << "↳ 〔 ✍  Description 〕: ";
+    std::getline(std::cin, *projectDescription);
+
+    std::cout << "↳ 〔 ♡ Author ♡ 〕: ";
+    std::getline(std::cin, *author);
+
+    std::cout << "↳ 〔 ❖ License ❖ 〕: ";
+    std::getline(std::cin, *license);
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/app/app.hpp"
+#include <memory>
+
+int main(int argc, char* argv[]) {
+    std::unique_ptr<App> app = std::make_unique<App>();
+    app->run();
+    return 0;
+}
